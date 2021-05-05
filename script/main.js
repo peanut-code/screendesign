372a1e74975a4e946847e2809e83959722d6e53d
Lina Dreher <ld038@hdm-stuttgart.de>
2021-05-05 16:10:03 +0200

    überflüssige script datei gelöscht

--- script/main.js
+++
@@ -1,9 +0,0 @@
-
- function check(input) {
-   if (!input.valu1.length>2) {
-     input.setCustomValidity("pleas fill it correctily");
-   } else {
-     // input is fine -- reset the error message
-     input.setCustomValidity('');
-   }
- }
